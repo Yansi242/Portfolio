2eecf1cb390347e0d538f31ba61ae5128735833c
Yansi Viramgama <yansi.v@shaligraminfotech.com>
2024-03-04 18:55:29 +0530

    changes

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,8 +1,33 @@
-import React from 'react'
+import {Suspense, useEffect,useState} from 'react';
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls,Preload,useGLTF } from '@react-three/drei';
+import CanvasLoader from '../Loader';
 
 const Computers = () => {
-  return (
-    <div>Computers</div>
+  const computer = useGLTF('./desktop_pc/scene.gltf')
+
+  return(
+      <mesh>
+        <hemisphereLight intensity={0.15} groundColor="black"/>
+        <pointLight intensity={1}/>
+        <primitive object={computer.scene}/>
+    </mesh>  
+  )
+}
+
+const ComputersCanvas = () => {
+  return(
+    <canvas frameLoop="demand" shadows camera={{position:[20,3,5],fov:25}} gl={{preserveDrawingBuffer : true}}>
+      <Suspense fallback={<CanvasLoader></CanvasLoader>}>
+        <OrbitControls 
+        enableZoom={false}
+        maxPolarAngle={Math.PI/2}
+        minPolarAngle={Math.PI/2}>         
+        </OrbitControls>
+        <computers></computers>
+      </Suspense>
+      <Preload all></Preload>
+    </canvas>
   )
 }
 
